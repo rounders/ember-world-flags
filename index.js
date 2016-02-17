dd2d7fd4b1788b3c413d58319f6f40ff2c9fafda
Francois Harbec <fharbec@gmail.com>
2016-02-17 13:24:52 -0800

    enable ember-world-flags to be used as nested addon

--- index.js
+++ index.js
@@ -3,8 +3,14 @@
 
 module.exports = {
   name: 'ember-world-flags',
-  included: function(app) {
-    app.import('vendor/flags16.css');
-    app.import('vendor/flags32.css');
+  included: function(app, parentAddon) {
+    if (typeof app.import !== 'function' && app.app) {
+      this.app = app = app.app;
+    }
+    this._super.included(app);
+    var target = (parentAddon || app);
+    
+    target.import('vendor/flags16.css');
+    target.import('vendor/flags32.css');
   }
 };
