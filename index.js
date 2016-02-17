c0cd873c39e91a8bb9461ddaae17b920c84db48c
Francois Harbec <fharbec@gmail.com>
2016-02-17 14:04:43 -0800

    add comment

--- index.js
+++ index.js
@@ -4,12 +4,15 @@
 module.exports = {
   name: 'ember-world-flags',
   included: function(app, parentAddon) {
+    // necessary for nested usage
+    // parent addon should call `this._super.included.apply(this, arguments);`
+    // see https://github.com/ember-cli/ember-cli/issues/3718
     if (typeof app.import !== 'function' && app.app) {
       this.app = app = app.app;
     }
     this._super.included(app);
     var target = (parentAddon || app);
-    
+
     target.import('vendor/flags16.css');
     target.import('vendor/flags32.css');
   }
