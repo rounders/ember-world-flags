748af68f250b38b4b86e453351f51e5bcfca010c
Francois Harbec <fharbec@gmail.com>
2020-06-28 15:10:53 -0700

    fix rootURL

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -40,9 +40,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.locationType = 'hash';
+    ENV.locationType = 'auto';
     ENV.rootURL = '/ember-world-flags/';
-    ENV.rootURL = '/ember-world-flags';
   }
 
   return ENV;
